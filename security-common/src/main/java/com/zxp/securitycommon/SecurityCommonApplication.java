3891aae4a4d18f3754da2df8c14522f6f3f866a5
zxp <451511792@qq.com>
2019-05-04 23:47:28 +0800

    second commit

---
+++ security-common/src/main/java/com/zxp/securitycommon/SecurityCommonApplication.java
@@ -0,0 +1,13 @@
+package com.zxp.securitycommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityCommonApplication.class, args);
+    }
+
+}
---
+++ security-common/src/test/java/com/zxp/securitycommon/SecurityCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zxp.securitycommon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityCommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
